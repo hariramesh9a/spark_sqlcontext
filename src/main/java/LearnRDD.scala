455f3e439b0a8c44320b0c50ba8f3b848524264a
ramesh2 <haryramesh@hotmail.com>
2016-05-22 13:21:12 -0400

    new chnaged # Please enter the commit message for your changes. Lines starting

--- src/main/java/LearnRDD.scala
+++ src/main/java/LearnRDD.scala
@@ -2,45 +2,81 @@ import org.apache.spark
 import org.apache.spark.SparkContext
 import scala.collection.mutable.Map
 import org.apache.spark.SparkConf
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.DriverManager;
+import org.apache.hive.jdbc.HiveDriver
 
 object LearnRDD {
 
   def main(args: Array[String]): Unit = {
-    
-    //var configMap:Map[String,String]=Map("FileName"->"src/main/resources/hello.txt","OutputFileName"-> "src/main/resources/output.csv")
 
+    //var configMap:Map[String,String]=Map("FileName"->"src/main/resources/hello.txt","OutputFileName"-> "src/main/resources/output.csv")
+    //Uncomment below comments one by one to see the results.
     //basics()
-    filterandcount()
+    //filterandcount()
+    //findMaxCity()
+    findCityandCountandPersistHiveRemotely()
   }
-  
+
   def basics(): Unit = {
     val sparkcon = new SparkContext("local[8]", "Learn RDD")
     var mydata = sparkcon.textFile("src/main/resources/employees.csv")
     val neededline = mydata.filter { line => line.contains("Shi") }
     val words = neededline.flatMap { x => x.split(",") }.filter { y => y.contains("Shi") }
     val count = words.map(x => (x, 1)).reduceByKey { case (a, b) => a + b }
-    count.coalesce(1).saveAsTextFile("src/main/resources/output")    
+    count.coalesce(1).saveAsTextFile("src/main/resources/output")
   }
-  
-  
-  def filterandcount(): Unit = {
-//    using error log in resources
-    val conf=new SparkConf().setAppName("Filter and Count").setMaster("local[8]")
-    val sparkcon=new SparkContext(conf)
-    val errorlog=sparkcon.textFile("src/main/resources/hello.txt")
-    val splitRDD=errorlog.flatMap { x => x.split(":")}
-    println(splitRDD.first().contains("error"))
 
-    val words=splitRDD.filter { x => x.contains("error") }
-    val countwords=words.map { (x) => (x,1) }
-    val counts=countwords.reduceByKey({case(x,y)=>(x+y)}).foreach(println)
-    
+  def filterandcount(): Unit = {
+    //    using error log in resources
+    val conf = new SparkConf().setAppName("Filter and Count").setMaster("local[8]").set("spark.cores.max", "10")
+    val sparkcon = new SparkContext(conf)
+    val errorlog = sparkcon.textFile("src/main/resources/hello.txt")
+    val errrlog = errorlog.filter { line => line.split(":")(0).contains("error") }
+    val splitRDD = errrlog.map { x => x.split(":")(0) + ':' + x.split(":")(2) }
+    val countwords = splitRDD.map { (x) => (x, 1) }
+    var nl = System.getProperty("line.separator")
+    val counts = countwords.reduceByKey({ case (x, y) => (x + y) }).foreach(println)
+    println("Hari" + nl + "Hari")
     //println("Number of errors: "+onlyerror.count())
     //now get key and values
-  
-    
-    
   }
+
+  //find most players born city just using RDD
+  def findMaxCity(): Unit = {
+    val conf = new SparkConf().setAppName("Player Cities").setMaster("local[8]")
+    val sc = new SparkContext(conf)
+    val mydata = sc.textFile("src/main/resources/Master.csv")
+    println(mydata.map(line => line.split(",")(6)).map { x => (x, 1) }.reduceByKey({ case (x, y) => x + y }).sortBy(f => f._2, false).first())
+  }
+
+  //eof most players born city
+
+  //Use spark and persisit to hive
+  def findCityandCountandPersistHiveRemotely(): Unit = {
+    val conf = new SparkConf().setAppName("Save result to hive remotely").setMaster("local[8]")
+    val sc = new SparkContext(conf)
+    val mydata = sc.textFile("src/main/resources/Master.csv")
+    val myRDD = mydata.map { mycity => mycity.split(",")(6) }.map { x => (x, 1) }.reduceByKey({ case (x, y) => x + y }).sortBy(f => f._2, true).sample(false, .001)
+    var drivername: String = "org.apache.hadoop.hive.jdbc.HiveDriver"
+    myRDD.foreachPartition {
+      mydata=>
+     var con: Connection = DriverManager.getConnection("jdbc:hive2://ehplbigi1002.nwie.net:2181,ehplbigi1003.nwie.net:2181,ehplbigi1005.nwie.net:2181/;serviceDiscoveryMode=zooKeeper;zooKeeperNamespace=hiveserver2", "ramesh2", "Ilovespark007")
+     var stmt=con.createStatement()
+      var tableName: String = "baseballplayers"
+      for (eachrow <-mydata)
+      {   
+        var query="insert into ramesh2.baseballplayers values ('"+ eachrow._1  +"','"+ eachrow._2 +"')"
+        stmt.executeUpdate(query)
+     }
+     con.close()
+    }
+  }
+
+  //eof use spark and persist to Hive
 
 }
 
--- src/main/java/SparkJoins.scala
+++ src/main/java/SparkJoins.scala
@@ -1,26 +1,52 @@
-
 import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SQLContext
 import org.apache.spark.sql.{Row, SQLContext}
 import org.apache.spark.sql.hive.HiveContext
 import org.apache.spark.sql.types.{ StructType, DateType, StringType, StructField, DoubleType }
 import org.apache.spark.sql.types.StringType
 import org.apache.spark.sql.types.StructField
+import org.apache.spark.sql.functions._
+import org.apache.spark.SparkConf
 
-
+//using spark sql join: join population.csv and countrycode.txt and see whihc country had max poulation in which year
 object SparkJoins {
-  
-  def main(args: Array[String]): Unit = {
-  
-    var sc= new SparkContext("local[4]","Connect to remote hive")
-    val data="src/main/resources/hello.txt"
-    val distData=sc.textFile(data)
-    val coll=distData.map(_.split(",")).map(r=>r(1))
-    println(coll.count())
-    coll.foreach(println)
-    println(coll.toDebugString)
 
-    
+  def main(args: Array[String]): Unit = {
+   
+  country_count()
   }
   
   
+   def country_count(): Unit = {
+     val conf=new SparkConf().setAppName("count countries").setMaster("local[8]")
+     val cont=new SparkContext(conf)
+     
+     var file=cont.textFile("src/main/resources/population.csv", 4)
+     var words=file.flatMap { x => x.split(",")(0) }.map { line => (line,1) }.reduceByKey{case(x,y)=>x+y}
+     println(words.first())    
+     
+     
+     
+     
+     
+     
+    
+  }
+   
+   def join():Unit={
+      val conf = new SparkConf().setMaster("local[8]").setAppName("Joins")
+    val sc = new SparkContext(conf)
+    val sqlContext= new SQLContext(sc)
+    val countryData = "src/main/resources/countrycode.txt"
+    val popData = "src/main/resources/population.csv"
+    val countrycodedata = sqlContext.read.format("com.databricks.spark.csv") 
+    val cd=countrycodedata.schema(StructType(Array(StructField("code",StringType,true),StructField("seq",StringType,false)))).load(countryData)//.registerTempTable("countrycode")
+    val cn=sqlContext.read.format("com.databricks.spark.csv").option("Header", "true").option("inferSchema", "true").load(popData)//.registerTempTable("pop")
+    println(cn.printSchema())
+    println(cd.printSchema())
+    cn.join(cd, cd.col("code").equalTo(cn("code"))).groupBy("country").agg(min("Value").as("min_val")).coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save("src/main/resources/test")
+  
+   }
+
 }
